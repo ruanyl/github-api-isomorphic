d43404b48afe66484c5dbb81d0e82ce644a924ac
ruanyl <ruanyu1@gmail.com>
2015-12-24 22:32:58 +0200

    change module structure

--- src/repos.js
+++ src/repos.js
@@ -1,68 +1,7 @@
-var request = require('superagent');
-var base64 = require('js-base64').Base64;
-var endpoint = require('./endpoint');
-
 module.exports = function(authMiddleware, user, repo) {
-  var url = endpoint.root + '/repos/' + user + '/' + repo + '/collaborators';
-
-  function listCollaborators(cb) {
-    request
-      .get(url)
-      .use(authMiddleware())
-      .end(function(err, res) {
-        if(err || !res.ok) {
-          cb(err);
-        } else {
-          cb(null, res.body);
-        }
-      });
-  }
-
-  function isCollaborator(username, cb) {
-    request
-      .get(url + '/' + username)
-      .use(authMiddleware())
-      .end(function(err, res) {
-        if(err && err.status === 404) {
-          cb(null, false);
-        } else if(res.status === 204) {
-          cb(null, true);
-        } else {
-          cb(err);
-        }
-      });
-  }
-
-  function addCollaborator(username, cb) {
-    request
-      .put(url + '/' + username)
-      .use(authMiddleware())
-      .end(function(err, res) {
-        if(err || !res.ok) {
-          cb(err);
-        } else if(res.status === 204) {
-          cb(null, true);
-        }
-      });
-  }
-
-  function removeCollaborator(username, cb) {
-    request
-      .delete(url + '/' + username)
-      .use(authMiddleware())
-      .end(function(err, res) {
-        if(err || !res.ok) {
-          cb(err);
-        } else if(res.status === 204) {
-          cb(null, true);
-        }
-      });
-  }
+  var reposCollaborators = require('./reposCollaborators')(authMiddleware, user, repo);
 
   return {
-    listCollaborators: listCollaborators,
-    isCollaborator: isCollaborator,
-    addCollaborator: addCollaborator,
-    removeCollaborator: removeCollaborator
+    ...reposCollaborators
   };
 };
---
+++ src/reposCollaborators.js
@@ -0,0 +1,68 @@
+var request = require('superagent');
+var endpoint = require('./endpoint');
+
+module.exports = function(authMiddleware, user, repo) {
+  var url = endpoint.root + '/repos/' + user + '/' + repo + '/collaborators';
+
+  function listCollaborators(cb) {
+    request
+      .get(url)
+      .use(authMiddleware())
+      .end(function(err, res) {
+        if(err || !res.ok) {
+          cb(err);
+        } else {
+          cb(null, res.body);
+        }
+      });
+  }
+
+  function isCollaborator(username, cb) {
+    request
+      .get(url + '/' + username)
+      .use(authMiddleware())
+      .end(function(err, res) {
+        if(err && err.status === 404) {
+          cb(null, false);
+        } else if(res.status === 204) {
+          cb(null, true);
+        } else {
+          cb(err);
+        }
+      });
+  }
+
+  // TODO support permission of organization repos
+  function addCollaborator(username, cb) {
+    request
+      .put(url + '/' + username)
+      .use(authMiddleware())
+      .end(function(err, res) {
+        if(err || !res.ok) {
+          cb(err);
+        } else if(res.status === 204) {
+          cb(null, true);
+        }
+      });
+  }
+
+  function removeCollaborator(username, cb) {
+    request
+      .delete(url + '/' + username)
+      .use(authMiddleware())
+      .end(function(err, res) {
+        if(err || !res.ok) {
+          cb(err);
+        } else if(res.status === 204) {
+          cb(null, true);
+        }
+      });
+  }
+
+  return {
+    listCollaborators: listCollaborators,
+    isCollaborator: isCollaborator,
+    addCollaborator: addCollaborator,
+    removeCollaborator: removeCollaborator
+  };
+};
