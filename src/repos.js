65ba4ee53996cf4ba58d304e66bf0e77e8002ca0
ruanyl <ruanyu1@gmail.com>
2015-12-29 22:28:12 +0200

    api for deploy keys

--- src/repos.js
+++ src/repos.js
@@ -5,10 +5,12 @@ module.exports = function(authMiddleware, user, repo) {
   var reposCollaborators = require('./reposCollaborators')(authMiddleware, url);
   var reposComments = require('./reposComments')(authMiddleware, url);
   var reposCommits = require('./reposCommits')(authMiddleware, url);
+  var reposDeployKeys = require('./reposDeployKeys')(authMiddleware, url);
 
   return {
     ...reposCollaborators,
     ...reposComments,
-    ...reposCommits
+    ...reposCommits,
+    ...reposDeployKeys
   };
 };
---
+++ src/reposDeployKeys.js
@@ -0,0 +1,74 @@
+var request = require('superagent');
+
+module.exports = function(authMiddleware, url) {
+  function listDeployKeys(cb) {
+    const _url = `${url}/keys`;
+
+    request
+      .get(_url)
+      .use(authMiddleware())
+      .end(function(err, res) {
+        if(err || !res.ok) {
+          cb(err);
+        } else {
+          cb(null, res.body);
+        }
+      });
+  }
+
+  function getDeployKey(id, cb) {
+    const _url = `${url}/keys/${id}`;
+
+    request
+      .get(_url)
+      .use(authMiddleware())
+      .end(function(err, res) {
+        if(err || !res.ok) {
+          cb(err);
+        } else {
+          cb(null, res.body);
+        }
+      });
+  }
+
+  // Details: https://developer.github.com/v3/repos/keys/#add-a-new-deploy-key
+  function addDeployKey(data, cb) {
+    const _url = `${url}/keys`;
+
+    request
+      .post(_url)
+      .use(authMiddleware())
+      .send(data)
+      .end(function(err, res) {
+        if(err || !res.ok) {
+          cb(err);
+        } else {
+          cb(null, res.body);
+        }
+      });
+  }
+
+  function removeDeployKey(id, cb) {
+    const _url = `${url}/keys/${id}`;
+
+    request
+      .del(_url)
+      .use(authMiddleware())
+      .end(function(err, res) {
+        if(err || !res.ok) {
+          cb(err);
+        } else if(res.status === 204) {
+          cb(null, true);
+        } else {
+          cb(null, false);
+        }
+      });
+  }
+
+  return {
+    listDeployKeys,
+    getDeployKey,
+    addDeployKey,
+    removeDeployKey
+  };
+};
